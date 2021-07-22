20d111176b06901a8cc4f7f5b260802905f5990c
Anirudh S <anirudh6626@gmail.com>
2021-07-22 14:11:56 +0530

    Changed sample tests

--- sample_tests/polonius_info_1169.rs
+++
@@ -1,8 +0,0 @@
-// thread 'rustc' panicked at 'no entry found for key'
-// prusti-interface/src/environment/polonius_info.rs:1169:9
-
-fn foo(p: *const i32) {
-    let _ = p.is_null();
-}
-
-fn main() {}
